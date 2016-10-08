6eedce4b3cbd02dc12130187d703185be0946533
Guna <gunas.yes@gmail.com>
2016-10-08 17:38:59 +0530

    cake_last_update_08_10

--- v2/js/script.js
+++ v2/js/script.js
@@ -1,7 +1,7 @@
 //Javascript 
 $(document).ready(function(){
 	//Navigation
-	 $('.profile-bg').height($(window).height() - 260);
+	 //$('.profile-bg').height($(window).height() - 260);
 	$('#nav-icon3').click(function(){
 		$(this).toggleClass('open');
 	});
@@ -9,9 +9,17 @@ $(document).ready(function(){
 /* For Browser Height*/
 	function resizeSlide() {
 		var fullHeight = $(window).height();
-		$("#wrapper, aside, #showcase, .cake-view-wrap, .cake-text-wrap").css({
+		$(".Wrapper-home, aside, #showcase").css({
 			'height' : fullHeight,
 		});
+		if (window.matchMedia('(min-width: 1400px)').matches) {
+			$(".cake-view-wrap, .cake-text-wrap").css({
+				'height': fullHeight - 63,
+			});
+		};
+		if (window.matchMedia('(max-width: 1399px)').matches) {
+			$(".cake-view-wrap").height($(".cake-text-wrap").height());
+		};
 	};
 	resizeSlide();
 	$( window ).resize(function() {
